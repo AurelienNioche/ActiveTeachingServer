8d6d4b38a8a1b6daf9ec6d08cdc20f28949deab9
ctorr148 <ctorre148@gmail.com>
2019-09-02 20:35:46 +0300

    no server files

---
+++ no_server/teaching_material/selection.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+from teaching_material.models import Kanji
+
+try:
+    kanji, meaning = \
+        np.array(
+            Kanji.objects.values_list('kanji', 'meaning').order_by('index')
+        ).T
+
+    unique_meaning, ___idx___, __inverse__ = np.unique(meaning,
+                                                       return_index=True,
+                                                       return_inverse=True)
+
+    id_kanji = np.arange(len(kanji))
+    id_meaning = id_kanji[___idx___][__inverse__]
+
+except:
+    print("Cannot load the database content")
+    kanji = None
+    meaning = None
+
+# assert len(id_kanji) == len(id_meaning)
+# for i in range(len(id_kanji)):
+#     assert meaning[id_meaning[i]] == meaning[i]
+
+# print(kanji)
+# print(meaning)
+
+
+def total_number_of_items():
+    return Kanji.objects.count()
+
+
+def get_string_representation(id_question, id_possible_replies):
+
+    question = kanji[id_question]
+    possible_replies = [meaning[i] for i in id_possible_replies]
+    return question, possible_replies
+
+
+def get_id():
+
+    return id_kanji, id_meaning
