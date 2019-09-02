807ea875676c291a812205f253f2f6619668e266
ctorr148 <ctorre148@gmail.com>
2019-09-02 17:48:04 +0300

    user pickle files

--- analysis/tools/history.py
+++ analysis/tools/history.py
@@ -1,10 +1,26 @@
+import os
+import pickle
+
 import numpy as np
 
-from user_data.models import Question
 from teaching_material.selection import kanji
+from user_data.models import Question
+
+BKP_FOLDER = os.path.join("data", "Pilot20190902", "pickle")
+
+os.makedirs(BKP_FOLDER, exist_ok=True)
 
 
 def get(user_id):
+
+    bkp_file = os.path.join(BKP_FOLDER, f"user_{user_id}.p")
+
+    if os.path.exists(bkp_file):
+
+        print("Loading from pickle file...")
+        hist_question, hist_success, seen = \
+            pickle.load(open(bkp_file, 'rb'))
+        return hist_question, hist_success, seen
 
     n_item = len(kanji)
 
@@ -29,5 +45,8 @@ def get(user_id):
 
         seen[question, t:] = True
         # print("t", t, "n_seen", np.sum(seen[:, t]))
+
+    pickle.dump((hist_question, hist_success, seen),
+                open(bkp_file, 'wb'))
 
     return hist_question, hist_success, seen
