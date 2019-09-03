57c81a1e99c7fefa96c4b7672866a59587f0f165
ctorr148 <ctorre148@gmail.com>
2019-09-03 11:25:11 +0300

    update

--- analysis/similarity/semantic/measure.py
+++ analysis/similarity/semantic/measure.py
@@ -47,9 +47,9 @@ def get(word_list, normalize_similarity=True):
 
         data, loaded_word_list, loaded_normalize = \
             pickle.load(open(BKP_FILE, 'rb'))
-        if (loaded_word_list == np.array(word_list)).all() \
-                and loaded_normalize == normalize_similarity:
-            return data
+        # if (loaded_word_list == np.array(word_list)).all() \
+        #         and loaded_normalize == normalize_similarity:
+        return data
 
     data = _compute(word_list=word_list, normalize_similarity=True)
     pickle.dump((data, word_list, normalize_similarity), open(BKP_FILE, 'wb'))
