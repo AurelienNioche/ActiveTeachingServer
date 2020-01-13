a2b2e78b06c1b6713dfd812517a6d7eeb36497a9
AurelienNioche <nioche.aurelien@gmail.com>
2020-01-13 18:58:28 +0200

    update

--- core/consumers.py
+++ core/consumers.py
@@ -18,11 +18,23 @@ class WebSocketConsumer(WebsocketConsumer):
 
     def receive(self, text_data=None, bytes_data=None):
 
-        text_data_json = json.loads(text_data)
+        if bytes_data is not None:
+            text = bytes_data.decode("utf-8")
+        elif text_data is not None:
+            text = text_data
+        else:
+            raise ValueError
+
+        text_data_json = json.loads(text)
+
         print("Receive:", text_data_json)
         # message = text_data_json['message']
 
         question = get_question(text_data_json)
 
         print("Send:", question)
-        self.send(text_data=json.dumps(question))
+        resp = json.dumps(question)
+        if bytes_data is not None:
+            self.send(bytes_data=resp.encode())
+        else:
+            self.send(text_data=resp)
