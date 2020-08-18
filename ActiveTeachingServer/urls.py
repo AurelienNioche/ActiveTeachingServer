21215c1a8cce0e2e1d5f19267b4a154a4ea522dc
Aurélien Nioche <nioche.aurelien@gmail.com>
2020-08-18 17:38:50 +0300

    update

--- ActiveTeachingServer/urls.py
+++ ActiveTeachingServer/urls.py
@@ -23,6 +23,6 @@ from django.urls import include, path
 urlpatterns = [
     # path('hey', admin.site.urls),
     path('admin/', admin.site.urls),
-    # path('', include('graphic_interface.urls')),
+    path('', include('graphic_interface.urls')),
     # url(r'^$', RedirectView.as_view(url='/admin')),
 ]
