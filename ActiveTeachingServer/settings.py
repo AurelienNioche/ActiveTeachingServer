75bc3abf39f30baaa933c98a03872dd3b8828b32
Aurélien Nioche <nioche.aurelien@gmail.com>
2020-09-22 15:53:59 +0300

    update

--- ActiveTeachingServer/settings.py
+++ ActiveTeachingServer/settings.py
@@ -143,6 +143,15 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 
+LOGGING = {'loggers': {
+    'daphne': {
+        'handlers': [
+            'console',
+        ],
+        'level': 'DEBUG'
+    }, }
+}
+
 # os.makedirs('tmp', exist_ok=True)
 #
 # LOGGING = {
